b84089c0e4a693ef1fdda6b8c64e9f0e0c87c329
victor@victor <victor.dmaina@gmail.com>
2024-03-14 18:40:37 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const element = document.getElementById("main");
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+const yourName = "Victor Maina";
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
